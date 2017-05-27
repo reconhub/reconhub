dfe74232aa3de02fe3c36d9703ae2df254cc0912
Dirk Schumacher <mail@dirk-schumacher.net>
2017-05-27 12:28:12 +0200

    Fix tests for installing packages

--- tests/testthat/test-reconhub.R
+++ tests/testthat/test-reconhub.R
@@ -6,6 +6,8 @@ test_that("packages get attached automatically", {
 })
 
 test_that("install all dev versions of recon packages", {
+  # also see https://stackoverflow.com/a/27994299/2798441
+  Sys.setenv("R_TESTS" = "") # fix
   expect_message(install_dev_versions())
   # just checking one package
   expect_true("distcrete" %in% installed.packages())
